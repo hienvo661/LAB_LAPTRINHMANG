b71b5061760fb66b7d62ed088d2b8a58eb54120f
Võ Ngọc Hiện <63461448+hienvo661@users.noreply.github.com>
2022-03-05 14:08:41 +0700

    Update BAI1.java

--- LAB2/src/BAI1.java
+++ LAB2/src/BAI1.java
@@ -1,3 +1,6 @@
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
 /**
  * 
  */
@@ -12,8 +15,25 @@ public class BAI1 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int a, b;
+		double nghiem;
+		//DecimalFormat decimalFormat = new DecimalFormat("#.##");  // làm tròn đến 2 chữ số thập phân
+		Scanner banphim = new Scanner(System.in);
+		System.out.println("Nhập vào số a: ");
+		a = banphim.nextInt();
+		System.out.println("Nhập vào số b: ");
+		b = banphim.nextInt();
+		if (a==0) {
+			if (b==0) {
+				System.out.println("Phương trình này có vô số nghiệm.");
+			}
+			else {
+				System.out.println("Phương trình vô nghiệm.");
+				}
+			}
+		else {
+			nghiem = (double) -b/a; //ép kiểu cho ra nghiệm chính xác 
+			System.out.println("Phương trình có nghiệm x= " +nghiem);
+		}
 	}
-
 }
