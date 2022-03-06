c96f270354f4bc4f625328d58cf4d42103a9b636
Võ Ngọc Hiện <63461448+hienvo661@users.noreply.github.com>
2022-03-06 12:28:57 +0700

    Update BAI1.java

--- LAB3/src/BAI1.java
+++ LAB3/src/BAI1.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * 
  */
@@ -7,12 +9,34 @@
  *
  */
 public class BAI1 {
+	static int n;
+	static boolean ok = true;
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner banphim = new Scanner(System.in);
+		System.out.println("Nhập vào một số bất kì: ");
+		n = banphim.nextInt();
+	
+		for (int i=2;i < n-1; i++)
+		{
+			if (n % i==0) 
+			{
+				ok = false;
+				break;
+			} 
+			i++;
+		}
+		if (ok == true) 
+		{
+			System.out.println( "Số " +n+ " là số nguyên tố");
+		}else {
+			System.out.println("Số " +n+ " không phải là số nguyên tố ");
+		}
+		
 
 	}
 
