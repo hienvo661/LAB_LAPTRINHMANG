4f041e57de97121ec42501907913b7245d15f260
Võ Ngọc Hiện <63461448+hienvo661@users.noreply.github.com>
2022-03-06 12:51:09 +0700

    Create BAI3.java

---
+++ LAB3/src/BAI3.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author hienv
+ *
+ */
+public class BAI3 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
