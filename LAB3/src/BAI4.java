7e8e66ea0fa66ac80658a2ac633b776a8f72d3d4
Võ Ngọc Hiện <63461448+hienvo661@users.noreply.github.com>
2022-03-06 14:06:53 +0700

    Update BAI4.java

--- LAB3/src/BAI4.java
+++ LAB3/src/BAI4.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * 
  */
@@ -7,13 +9,40 @@
  *
  */
 public class BAI4 {
-
+	static String Hoten;
+	static double DTB;
+	public void nhap() 
+	{
+		Scanner banphim = new Scanner(System.in);
+		System.out.println("Nhập vào họ và tên: ");
+		Hoten = banphim.nextLine();
+		
+		System.out.println("Điểm trung bình: ");
+		DTB = banphim.nextDouble();
+		
+	}
+	public void xuat() 
+	{
+		System.out.println("\n ----------------------");
+		System.out.printf("Họ và tên sinh viên: %s \nĐiểm trung bình: %f \nHọc lực: %s", Hoten, DTB, xeploai());
+	}
+	public String xeploai()
+	{
+		if (DTB >= 9 )
+			return "Xuất sắc";
+		else 
+			return "Giỏi";
+		
+	}
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		BAI4 sv = new BAI4();
+		sv.nhap();
+		sv.xuat();
+		sv.xeploai();
 	}
 
 }
