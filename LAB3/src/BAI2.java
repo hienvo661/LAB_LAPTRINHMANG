e3a3f65f41852876274fbdfc000e98b2e2216dc0
Võ Ngọc Hiện <63461448+hienvo661@users.noreply.github.com>
2022-03-06 12:51:05 +0700

    Update BAI2.java

--- LAB3/src/BAI2.java
+++ LAB3/src/BAI2.java
@@ -7,12 +7,23 @@
  *
  */
 public class BAI2 {
+	static int x,i;
+	static int ketqua;
+	
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		for (x=2;x<=10;x++)
+		{
+			for(i=1;i<=10;i++) {
+				ketqua = x * i;
+				System.out.printf("%d x %d =%d \n", x, i, ketqua);
+			}
+			
+		}
 
 	}
 
