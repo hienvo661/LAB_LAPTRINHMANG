f87788a539c84bba9dc9987731c43271bd359aa3
Võ Ngọc Hiện <63461448+hienvo661@users.noreply.github.com>
2022-03-06 12:29:14 +0700

    Create BAI2.java

---
+++ LAB3/src/BAI2.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author hienv
+ *
+ */
+public class BAI2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
