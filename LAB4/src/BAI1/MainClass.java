cdda42386e4343704e1e6666bbe5226e8216a27c
Võ Ngọc Hiện <63461448+hienvo661@users.noreply.github.com>
2022-03-07 19:21:38 +0700

    Update MainClass.java

--- LAB4/src/BAI1/MainClass.java
+++ LAB4/src/BAI1/MainClass.java
@@ -11,7 +11,18 @@ public class MainClass {
 		System.out.println("Tên sản phẩm: " +ten);
 		sp1.setTenSP("number one");
 		//-------------------------------
+		double gia = sp1.getDongia();
+		System.out.println("Giá sản phẩm: " +gia);
+		sp1.setDongia(20000);
 		
+		
+		String tensp2 = sp2.getTenSP(); 
+		System.out.println("Tên sản phẩm: " +tensp2);
+		sp2.setTenSP("strongbow");
+		//-------------------------------
+		double giasp2 = sp2.getDongia();
+		System.out.println("Giá sản phẩm: " +giasp2);
+		sp2.setDongia(21000);
 //		sp1.nhap();
 //		sp2.nhap();
 		// xuất dữ liệu
