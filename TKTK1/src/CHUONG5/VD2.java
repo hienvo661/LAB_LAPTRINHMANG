7813137096bf81c326df9c9b10e877bf5a86d887
Võ Ngọc Hiện <63461448+hienvo661@users.noreply.github.com>
2022-03-09 18:20:29 +0700

    Create VD2.java

---
+++ TKTK1/src/CHUONG5/VD2.java
@@ -0,0 +1,14 @@
+package CHUONG5;
+import java.io.*;
+
+public class VD2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// xuất dữ liệu ra màn hình console bằng System.out.write();
+		int b = 'M';
+		System.out.write(b);
+		System.out.write('\n');
+	}
+
+}
