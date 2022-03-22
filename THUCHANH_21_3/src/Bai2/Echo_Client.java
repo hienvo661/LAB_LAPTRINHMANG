f99f760d62caa06b8eb553674f827f352091b773
Võ Ngọc Hiện <63461448+hienvo661@users.noreply.github.com>
2022-03-22 18:09:22 +0700

    Gửi nhận client - server

--- THUCHANH_21_3/src/Bai2/Echo_Client.java
+++ THUCHANH_21_3/src/Bai2/Echo_Client.java
@@ -3,6 +3,16 @@
  */
 package Bai2;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
 /**
  * @author hienv
  *
@@ -14,7 +24,38 @@ public class Echo_Client {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Socket soc;
+		try {
+			soc = new Socket("localhost", 9999);
+			System.out.println("Connect ..... ");
+			// lấy luồng
+			InputStream input = soc.getInputStream();
+			InputStreamReader inputReader = new InputStreamReader(input);
+			BufferedReader buffRead = new BufferedReader(inputReader);
+			
+			OutputStream osToClient = soc.getOutputStream();
+			OutputStreamWriter Write2client = new OutputStreamWriter(osToClient);
+					
+			BufferedWriter buffWrite = new BufferedWriter(Write2client);
+			
+			// lấy chuỗi từ bàn phím
+			Scanner banphim = new Scanner(System.in);
+			while(true) {
+				System.out.println("\nClient gửi messenger:  ");
+				String chuoiGui = banphim.nextLine();
+				// gửi đi nếu chuỗi không đúng -> in ra Bye!!!
+				buffWrite.write(chuoiGui+ "\n");
+				buffWrite.flush();
+				// nhận về
+				String chuoiNhan = buffRead.readLine();
+				System.out.println("Server: "+chuoiNhan);
+				if(chuoiGui.equals("Bye!!!")); break;
+			}
+			soc.close();
+//			System.out.println(buffRead.readLine());
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
-
 }
--- THUCHANH_21_3/src/Bai2/Echo_Server.java
+++ THUCHANH_21_3/src/Bai2/Echo_Server.java
@@ -3,6 +3,16 @@
  */
 package Bai2;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  * @author hienv
  *
@@ -14,7 +24,43 @@ public class Echo_Server {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		try {
+			// mở cổng và bắt đầu nghe
+			ServerSocket socketServer = new ServerSocket(9999);
+			System.out.println("Đang lắm nghe cổng 9999 ..... ");
+			//luôn luôn lắng nghe bằng vòng lặp while không giới hạn
+			while(true)
+			{
+				//accept khi có người gửi yêu cầu đến server
+				Socket socketClient = socketServer.accept();
+				// lấy về luồng nhập, xuất
+				OutputStream osToClient = socketClient.getOutputStream();
+				OutputStreamWriter write2Client = new OutputStreamWriter(osToClient);
+				BufferedWriter buffWriter = new BufferedWriter(write2Client);
+				
+				InputStream input = socketClient.getInputStream();
+				InputStreamReader inputReader = new InputStreamReader(input);
+				BufferedReader BuffRead = new BufferedReader(inputReader); 
+//				// gửi 			
+//				bufferWriter.write("Xin Chào Client \n");
+//				bufferWriter.flush();
+//				// đóng socket
+//				socketClient.close();
+				while(true) 
+				{
+					// nhận dữ liệu 
+					String chuoinhan = BuffRead.readLine();
+					String chuoiGui = chuoinhan;
+					buffWriter.write(chuoiGui+"\n");
+					buffWriter.flush();
+					if(chuoiGui.equals("Bye!!!")); break;
+					
+				}
+				socketClient.close();
+			}
+		}catch(IOException e){
+			e.printStackTrace();
+		}
 	}
 
 }
