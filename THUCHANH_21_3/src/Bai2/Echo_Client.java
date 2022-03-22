7dd0df62e274b08ac023a7daf16ed3d596a06bd7
Võ Ngọc Hiện <63461448+hienvo661@users.noreply.github.com>
2022-03-22 13:01:47 +0700

    thuchanh_21/3
    bài thực hành Echo Socket 21/3/2022

---
+++ THUCHANH_21_3/src/Bai2/Echo_Client.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package Bai2;
+
+/**
+ * @author hienv
+ *
+ */
+public class Echo_Client {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
