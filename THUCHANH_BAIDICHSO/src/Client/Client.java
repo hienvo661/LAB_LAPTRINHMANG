88f968330646a862bda81eb827c1e948dfd7e6eb
Võ Ngọc Hiện <63461448+hienvo661@users.noreply.github.com>
2022-04-04 18:51:20 +0700

    Thực hành 4/4/2022

---
+++ THUCHANH_BAIDICHSO/src/Client/Client.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package Client;
+
+/**
+ * @author hienv
+ *
+ */
+public class Client {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ THUCHANH_BAIDICHSO/src/Server/Server.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package Server;
+
+/**
+ * @author hienv
+ *
+ */
+public class Server {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
