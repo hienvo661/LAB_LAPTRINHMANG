b94701a00fe1f619327b6f167b792341554e9833
Võ Ngọc Hiện <63461448+hienvo661@users.noreply.github.com>
2022-04-04 18:55:00 +0700

    connect

--- THUCHANH_BAIDICHSO/src/Client/Client.java
+++ THUCHANH_BAIDICHSO/src/Client/Client.java
@@ -3,6 +3,15 @@
  */
 package Client;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
 /**
  * @author hienv
  *
@@ -14,6 +23,26 @@ public class Client {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		try {
+			Socket soc = new Socket("localhost",8989);
+			System.out.print("Conected!\n");
+			
+			InputStream in = soc.getInputStream();
+			InputStreamReader inReader = new InputStreamReader(in);
+			BufferedReader buffR = new BufferedReader(inReader);
+
+			OutputStream osToClient = soc.getOutputStream();	
+			OutputStreamWriter write2Client = new OutputStreamWriter(osToClient);
+			BufferedWriter buffW = new BufferedWriter(write2Client);
+			
+			Scanner banPhim = new Scanner(System.in);
+			//=====THIẾT KẾ GIAO THỨC ======
+			
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
 
 	}
 
