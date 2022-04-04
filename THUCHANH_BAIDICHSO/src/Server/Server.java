39dad14ed0cc7f1b44695cc8150cf7ec31e83d5e
Võ Ngọc Hiện <63461448+hienvo661@users.noreply.github.com>
2022-04-04 20:04:30 +0700

    Update Server.java

--- THUCHANH_BAIDICHSO/src/Server/Server.java
+++ THUCHANH_BAIDICHSO/src/Server/Server.java
@@ -93,7 +93,7 @@ public class Server extends Thread {
 						String chuoiGui= "ten";
 						buffW.write(chuoiGui+"\n");
 						buffW.flush();
-						if(chuoiGui.equals("10")) break;
+						if(chuoiGui.equals("0")) break;
 					}
 				}
 				
