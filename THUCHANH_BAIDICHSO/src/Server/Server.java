e90e31a86ecc41e257d8e1623ba9fad32d41449e
Võ Ngọc Hiện <63461448+hienvo661@users.noreply.github.com>
2022-04-04 19:22:22 +0700

    Update Server.java

--- THUCHANH_BAIDICHSO/src/Server/Server.java
+++ THUCHANH_BAIDICHSO/src/Server/Server.java
@@ -45,14 +45,39 @@ public class Server extends Thread {
 			InputStreamReader inReader = new InputStreamReader(in);
 			BufferedReader buffR = new BufferedReader(inReader);
 			while(true) {
-
+			String chuoiNhan = buffR.readLine();
+			//Gửi trả
+				switch (chuoiNhan) {
+				  case "0":
+				    System.out.println("Zero");
+				  case "1":
+				    System.out.println("One");
+				  case "2":
+				    System.out.println("Two");
+				  case "3":
+				    System.out.println("Three");
+				  case "4":
+				    System.out.println("Four");
+				  case "5":
+				    System.out.println("Five");
+				  case "6":
+				    System.out.println("Six");
+				  case "7":
+					System.out.println("Seven");
+				  case "8":
+					System.out.println("Eight");
+				  case "9":
+					System.out.println("Nine");
+				  case "10":
+					System.out.println("Ten");
+					break;
 			}
 			socketClient.close();
-		} catch (Exception e) {
+			}
+			catch (Exception e) {
 			// TODO: handle exception
 			System.err.print(e.getMessage());
 		}
 	
-	}
 }
 
